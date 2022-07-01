7b43f997cb52581d180aef59a2c7f915ebfb2528
Arch0125 <dasarchisman25@gmail.com>
2022-07-02 03:15:41 +0530

    Add Borrow UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import Borrow from './components/Borrow';
 
 function App() {
   return (
     <div className='flex flex-col pt-[5%] items-center w-[100vw] h-[100vh] bg-gradient-to-b from-[#1a0636] to-[#060424] ' >
-      <Tabs variant={'solid-rounded'} colorScheme='purple'>
+      <Tabs variant={'solid-rounded'} colorScheme='purple' width={"40%"}align={'center'} >
         <TabList>
           <Tab>Borrow</Tab>
           <Tab>Lend</Tab>
           <Tab>Swap</Tab>
           <Tab>Portfolio</Tab>
         </TabList>
-        <TabPanels>
+        <TabPanels  >
           <TabPanel>
-            <p>one!</p>
+            <Borrow/>
           </TabPanel>
           <TabPanel>
             <p>two!</p>
---
+++ src/components/Borrow.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
+const Borrow = () => {
+    return ( 
+        <div className=' flex flex-col justify-center w-[100%] h-[100%] bg-[#050229] rounded-3xl p-[20px] ' >
+            <p className='text-white font-bold ' >Select Pool</p>
+            <Tabs mt={"20px"} variant='solid-rounded'   width={"100%"}>
+            <TabList  >
+                <Tab _selected={{bg:"#1a0636"}} width={"50%"} ><img src='https://svgshare.com/i/iis.svg' width={"40px"} className='mr-3' />MATIC-USDC</Tab>
+                <Tab _selected={{bg:"#1a0636"}} width={"50%"} ><img src='https://svgshare.com/i/iiV.svg' width={"40px"} className='mr-3' />WETH-USDC</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                <p>one!</p>
+                </TabPanel>
+                <TabPanel>
+                <p>two!</p>
+                </TabPanel>
+            </TabPanels>
+            </Tabs>
+        </div>
+     );
+}
+ 
+export default Borrow;
