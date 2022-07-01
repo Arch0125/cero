4dc8bb716caf8be6afd9c1ef63ffa66ef7ab3633
Arch0125 <dasarchisman25@gmail.com>
2022-07-02 04:12:46 +0530

    Update UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className='flex flex-row justify-between w-[100vw] h-[fit-content] p-2 bg-[#1a0636] ' >
             <div className='flex flex-row' >
             <img src='https://svgshare.com/i/iiW.svg' className='w-[20px]' />
-            <label className='font-bold text-white text-2xl ml-2 mt-1'>CERO</label>
+            <label className='font-thin text-white text-2xl ml-2 mt-1'>CERO</label>
             </div>
             <ConnectButton/>
         </div>
