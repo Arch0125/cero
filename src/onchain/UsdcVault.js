ae17de7cb2d4d6eef93ec3cae4fa9f0a442ed384
Arch0125 <dasarchisman25@gmail.com>
2022-07-03 23:10:28 +0530

    Revert Back

--- src/onchain/UsdcVault.js
+++ src/onchain/UsdcVault.js
@@ -7,30 +7,30 @@ import GetUSDC from '../hooks/GetUSDC'
 
 const UsdcVault = () => {
 
-    const usdcvault = GetUSDCVault();
-    const usdc =  GetUSDC();
-    const[bal,setBal]=useState('');
-    const[totalSupply,setTotalSupply]=useState('')
-    const[deposit,setDeposit]=useState('');
-    var addr = GetAccount();
-    addr = addr.toString();
+    // const usdcvault = GetUSDCVault();
+    // const usdc =  GetUSDC();
+    // const[bal,setBal]=useState('');
+    // const[totalSupply,setTotalSupply]=useState('')
+    // const[deposit,setDeposit]=useState('');
+    // var addr = GetAccount();
+    // addr = addr.toString();
 
-    const depositTo=async(deposit)=>{
-        await usdc.approve('0x3F84668d2AF41D150546f5cd5bd3f8f1DE88669E',(100*decimals).toString())
-        await usdcvault.deposit((deposit*decimals).toString()).then(console.log);
-    }
+    // const depositTo=async(deposit)=>{
+    //     await usdc.approve('0x3F84668d2AF41D150546f5cd5bd3f8f1DE88669E',(100*decimals).toString())
+    //     await usdcvault.deposit((deposit*decimals).toString()).then(console.log);
+    // }
 
-    const getBalance=async()=>{
-        const bigno = await usdcvault.balanceOf(addr);
-        var tSupply = await usdcvault.totalSupply();
-        tSupply=tSupply.toString() /decimals;
-        var res = bigno.toString();
-        res = res/decimals;
-        setBal(res);
-        setTotalSupply(tSupply);
-    }
+    // const getBalance=async()=>{
+    //     const bigno = await usdcvault.balanceOf(addr);
+    //     var tSupply = await usdcvault.totalSupply();
+    //     tSupply=tSupply.toString() /decimals;
+    //     var res = bigno.toString();
+    //     res = res/decimals;
+    //     setBal(res);
+    //     setTotalSupply(tSupply);
+    // }
 
-    getBalance();
+    // getBalance();
 
     return ( 
         <div className=' flex flex-col w-[100%] h-[100%] bg-[rgb(26,6,54)] p-[15px] rounded-2xl mt-6 ' >
@@ -39,15 +39,15 @@ const UsdcVault = () => {
                 <label className='text-white font-bold ' >TVL : </label>
             </div>
             <div className='flex flex-row justify-between px-2 mt-2 mb-3' >
-                <label className='text-white  ' >Total Supply : {totalSupply} USDC</label>
-                <label className='text-white  ' >Your Supply : {bal} USDC</label>
+                <label className='text-white  ' >Total Supply : USDC</label>
+                <label className='text-white  ' >Your Supply : USDC</label>
             </div>
             <Divider/>
             <div className='flex flex-row justify-between px-2 mt-3 ' >
                 <InputGroup>
                 <InputLeftElement><img src='https://cryptologos.cc/logos/usd-coin-usdc-logo.svg?v=022' className='w-[20px]' /></InputLeftElement>
-                <Input onChange={e=>setDeposit(e.target.value)} className='text-gray-400' borderColor={'purple.900'}/>
-                <InputRightAddon onClick={()=>depositTo(deposit)} className='text-white cursor-pointer' bg={'purple.900'} border={'none'} >Deposit</InputRightAddon>
+                <Input className='text-gray-400' borderColor={'purple.900'}/>
+                <InputRightAddon className='text-white cursor-pointer' bg={'purple.900'} border={'none'} >Deposit</InputRightAddon>
                 </InputGroup>
             </div>
         </div>
