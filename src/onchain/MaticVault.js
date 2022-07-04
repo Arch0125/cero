aaa643049eb62d1d8864e4d63d76598419df3e8f
Arch0125 <dasarchisman25@gmail.com>
2022-07-04 08:41:34 +0530

    Fix UI

--- src/onchain/MaticVault.js
+++ src/onchain/MaticVault.js
@@ -9,6 +9,7 @@ const MaticVault = () => {
     const wmatic = GetWMATIC();
     const wmaticvault=GetUSDCVault();
     const[totalSupply,setTotalSupply]=useState('');
+    const[tvl,setTvl]=useState(0);
 
     useEffect(()=>{
         getBalance();
@@ -18,6 +19,7 @@ const MaticVault = () => {
         var tSupply = await wmaticvault.totalSupply();
         tSupply = (tSupply/decimals).toString();
         setTotalSupply(tSupply);
+        setTvl(tSupply*0.45)
     }
 
 
@@ -25,10 +27,10 @@ const MaticVault = () => {
         <div className=' flex flex-col w-[100%] h-[100%] bg-[rgb(26,6,54)] p-[15px] rounded-2xl ' >
             <div className='flex flex-row justify-between px-2' >
                 <label className='text-white font-bold ' >MATIC Vault</label>
-                <label className='text-white font-bold ' >TVL : </label>
+                <label className='text-white font-bold ' >TVL : $ {tvl}</label>
             </div>
             <div className='flex flex-row justify-between px-2 mt-2 mb-3' >
-                <label className='text-white  ' >Total Supply : {totalSupply}</label>
+                <label className='text-white  ' >Total Supply : {totalSupply} MATIC</label>
                 
             </div>
             <Divider/>
