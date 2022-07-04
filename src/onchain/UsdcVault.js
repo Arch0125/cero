966bbd755a9d5b0201f1294a84ad65f356001f0b
Arch0125 <dasarchisman25@gmail.com>
2022-07-04 08:43:30 +0530

    Fix USDC Vault

--- src/onchain/UsdcVault.js
+++ src/onchain/UsdcVault.js
@@ -1,5 +1,5 @@
 import { Divider, Input, InputGroup, InputLeftElement, InputRightAddon } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import GetAccount from '../hooks/GetAccount';
 import GetUSDCVault from '../hooks/GetUSDCVault';
 import { decimals } from '../components/constants';
@@ -7,28 +7,32 @@ import GetUSDC from '../hooks/GetUSDC'
 
 const UsdcVault = () => {
 
-    // const usdcvault = GetUSDCVault();
-    // const usdc =  GetUSDC();
-    // const[bal,setBal]=useState('');
-    // const[totalSupply,setTotalSupply]=useState('')
-    // const[deposit,setDeposit]=useState('');
-    // var addr = GetAccount();
-    // addr = addr.toString();
+    useEffect(()=>{
+        getBalance();
+    })
 
-    // const depositTo=async(deposit)=>{
-    //     await usdc.approve('0x3F84668d2AF41D150546f5cd5bd3f8f1DE88669E',(100*decimals).toString())
-    //     await usdcvault.deposit((deposit*decimals).toString()).then(console.log);
-    // }
+    const usdcvault = GetUSDCVault();
+    const usdc =  GetUSDC();
+    const[bal,setBal]=useState('');
+    const[totalSupply,setTotalSupply]=useState('')
+    const[deposit,setDeposit]=useState('');
+    var addr = GetAccount();
+    addr = addr.toString();
 
-    // const getBalance=async()=>{
-    //     const bigno = await usdcvault.balanceOf(addr);
-    //     var tSupply = await usdcvault.totalSupply();
-    //     tSupply=tSupply.toString() /decimals;
-    //     var res = bigno.toString();
-    //     res = res/decimals;
-    //     setBal(res);
-    //     setTotalSupply(tSupply);
-    // }
+    const depositTo=async(deposit)=>{
+        await usdc.approve('0x3F84668d2AF41D150546f5cd5bd3f8f1DE88669E',(100*decimals).toString())
+        await usdcvault.deposit((deposit*decimals).toString()).then(console.log);
+    }
+
+    const getBalance=async()=>{
+        const bigno = await usdcvault.balanceOf(addr);
+        var tSupply = await usdcvault.totalSupply();
+        tSupply=tSupply.toString() /decimals;
+        var res = bigno.toString();
+        res = res/decimals;
+        setBal(res);
+        setTotalSupply(tSupply);
+    }
 
     // getBalance();
 
@@ -36,11 +40,11 @@ const UsdcVault = () => {
         <div className=' flex flex-col w-[100%] h-[100%] bg-[rgb(26,6,54)] p-[15px] rounded-2xl mt-6 ' >
             <div className='flex flex-row justify-between px-2' >
                 <label className='text-white font-bold ' >USDC Vault</label>
-                <label className='text-white font-bold ' >TVL : </label>
+                <label className='text-white font-bold ' >TVL : $ {totalSupply}</label>
             </div>
             <div className='flex flex-row justify-between px-2 mt-2 mb-3' >
-                <label className='text-white  ' >Total Supply : USDC</label>
-                <label className='text-white  ' >Your Supply : USDC</label>
+                <label className='text-white  ' >Total Supply : {totalSupply} USDC</label>
+                <label className='text-white  ' >Your Supply : {bal} USDC</label>
             </div>
             <Divider/>
             <div className='flex flex-row justify-between px-2 mt-3 ' >
