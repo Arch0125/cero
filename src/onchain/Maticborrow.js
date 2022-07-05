cf09674a4ff36e3789e1593b89a0afd9410fb68a
Arch0125 <dasarchisman25@gmail.com>
2022-07-05 22:18:36 +0530

    Update Price oracle

--- src/onchain/Maticborrow.js
+++ src/onchain/Maticborrow.js
@@ -14,6 +14,7 @@ import {
   import { InfoOutlineIcon } from '@chakra-ui/icons';
 import GetUSDC from '../hooks/GetUSDC';
 import GetUSDCVault from '../hooks/GetUSDCVault';
+import GetOracle from '../hooks/GetOracle';
 
 const Maticborrow = () => {
 
@@ -22,10 +23,12 @@ const Maticborrow = () => {
     useEffect(()=>{
         setColl(borrow*ratio/100);
         setRepay(borrow-coll);
+        getOracle();
     })
 
     const usdc = GetUSDC();
     const usdcvault = GetUSDCVault();
+    const oracle = GetOracle();
 
     const[matic,setMatic]=useState(0);
     const[borrow,setBorrow]=useState(0);
@@ -40,6 +43,13 @@ const Maticborrow = () => {
         await usdcvault.deposit((deposit*decimals).toString()).then(console.log);
     }
 
+    const getOracle=async()=>{
+        var big = new BigNumber(1).shiftedBy(18).toString();
+        var res = await oracle.estimateAmountOut('0xaa2ABC23B36E906cE603C6d19A88F0873A701b87',big,10);
+        let x = new BigNumber(`${res}`).shiftedBy(-18).toFixed(5);
+        setCurrentprice(x);
+    }
+
     return ( 
         <div className=' flex flex-col w-[100%] h-[100%] bg-[#1a0636] p-[15px] rounded-2xl ' >
             <p className='font-thin text-gray-400 '   >Borrow USDC to buy MATIC</p>
@@ -48,8 +58,8 @@ const Maticborrow = () => {
             <Input onChange={e=>{setMatic(e.target.value);setBorrow(e.target.value * currentprice * 1.03)}} placeholder='0.0' color={"gray.600"} mt={'5'} borderColor={'purple.900'} rounded={'2xl'} />
             </InputGroup>
             <div className='flex flex-row w-[100%] justify-between px-2 font-light mt-2 text-gray-500' >
-                <label>Current Price :  USDC</label>
-                <label>Borrow Amount : </label>
+                <label>Current Price : {currentprice} USDC</label>
+                <label>Borrow Amount : {borrow}</label>
             </div>
             <p className='font-thin text-gray-400 mt-2 '>Adjust Collateral</p>
             <Slider defaultValue={50} min={0} max={100} step={5} mt={2} onChange={(val)=>{setRatio(val);setApr(25/(val+5))}}  >
